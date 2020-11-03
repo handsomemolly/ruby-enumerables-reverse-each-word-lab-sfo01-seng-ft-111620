38f05e45e77844f3f58f421ceee969f19444e5f8
Ryan Kendig <ryan.kendig@gmail.com>
2020-11-03 18:43:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
-def reverse_each_word (str)
-  str.each do |letters|
-    letters.split()
-  end
+def reverse_each_word(sent1)
+  reversed = sent1.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
 end
